1608f17c45bbfb4bf3ee4115f02f74e04a1b813d
Kenechukwu Akubue <kengoon19@gmail.com>
2024-12-30 16:06:15 +0100

    Write test case for get-total-post

--- tests/techpost.test.ts
+++ tests/techpost.test.ts
@@ -1,8 +1,11 @@
 
+import { Cl } from "@stacks/transactions";
 import { describe, expect, it } from "vitest";
 
 const accounts = simnet.getAccounts();
+const deployer = accounts.get("deployer")!;
 const address1 = accounts.get("wallet_1")!;
+const address2 = accounts.get("wallet_2")!;
 
 /*
   The test below is an example. To learn more, read the testing documentation here:
@@ -19,3 +22,10 @@ describe("example tests", () => {
   //   expect(result).toBeUint(0);
   // });
 });
+
+describe("get-total-post function test", () => {
+  it("get's the total tech post made", () => {
+    const { result } = simnet.callReadOnlyFn("techpost", "get-total-post", [], address1)
+    expect(result).toBeUint(0)
+  })
+})
